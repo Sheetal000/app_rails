d60feedc7daa326d275661a994a3cf8e1b50b7fb
sheetalsheriwala <sheetalsheriwala@gmail.com>
2022-09-26 12:55:32 +0530

    currently working on in that files

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  Customer.find_each do |customer|
+    NewsMailer.weekly(customer).deliver_now
+  end  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_23_132340) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_25_170829) do
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "year_published"
+    t.string "isbn"
+    t.decimal "price"
+    t.boolean "out_of_print"
+    t.integer "supplier_id", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+    t.index ["supplier_id"], name: "index_books_on_supplier_id"
   end
 
   create_table "books_orders", id: false, force: :cascade do |t|
@@ -53,13 +67,30 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_132340) do
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "rating"
+    t.integer "state"
+    t.integer "customer_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["customer_id"], name: "index_reviews_on_customer_id"
+  end
+
   create_table "suppliers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "books", "authors"
+  add_foreign_key "books", "suppliers"
   add_foreign_key "books_orders", "books", column: "books_id"
   add_foreign_key "books_orders", "orders", column: "orders_id"
   add_foreign_key "orders", "customers"
+  add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "customers"
 end
